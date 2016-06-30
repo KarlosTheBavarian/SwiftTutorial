2f95b90aa7d069ce9284670d24d73bff31c399f7
KarlosTheBavarian <KarlosTheBavarian@users.noreply.github.com>
2016-06-30 18:29:07 +0200

    03_stringsVariablesAndConstants.swift
    Chapter 3 about Strings, Variables and Constants added

---
+++ 03_stringsVariablesAndConstants.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+str = "Swift is awesome!"
+
+str = "Concatenate " + "two strings"
+
+let anotherString = "This is another string"
+
+anotherString = "changing fails"
