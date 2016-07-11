a7982d2899a9f65230692c2b238c770435c1e1ab
Matthias Karl <matthias.karl@yahoo.com>
2016-07-11 18:09:49 +0200

    chapter 04: basic datatypes swiftfile added

---
+++ 04_basicDatatypes.swift
@@ -0,0 +1,55 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let language:String                         // String
+
+let firstCharacterOfLanguage:Character      // Character
+
+let introduced:Int                          // Integer
+
+var version:Double                          // Double
+
+let isAwesome:Bool                          // Boolean
+
+
+language = "Swift"
+
+firstCharacterOfLanguage = "S"
+
+introduced = 2014
+
+version = 2.3
+
+isAwesome = true
+
+
+var withOut = "Semicolon"
+
+var with = "Semicolon";
+
+
+let number1 = 5
+let number2 = 23
+
+var addition = 40 + 2
+addition = number1 + number2
+
+var subtraction = 48 - 6
+subtraction = number1 - number2
+
+var multiplication = 21 * 2
+multiplication = number1 * number2
+
+var division = 84/2
+division = number2/number1
+
+var modulo = 15%2
+modulo = number2%number1
+
+var result = 3 * 4 + 5
+result = 3 * (4 + 5)
+
+
+
+
